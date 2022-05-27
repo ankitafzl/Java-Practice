ad308753bf1d3e23f7d928c7a7e7a2368b80069b
ANKUSH <ANKUSH@192.168.0.194>
2022-05-27 22:54:54 +0530

    Practice

--- Java Practice/src/Hash/Subarray_Sums_Divisible_by_K.java
+++ Java Practice/src/Hash/Subarray_Sums_Divisible_by_K.java
@@ -1,5 +1,23 @@
 package Hash;
-
+import java.util.*;
 public class Subarray_Sums_Divisible_by_K {
 
+	 public int subarraysDivByK(int[] nums, int k) {
+	        HashMap<Integer,Integer> map=new HashMap<>();
+	        map.put(0,1);
+	        int run_sum=0;
+	        int count=0;
+	        for(int i=0;i<nums.length;i++){
+	            run_sum+=nums[i];
+	            int rem=run_sum%k;
+	            if(rem<0){
+	                rem+=k;
+	            }
+	            if(map.containsKey(rem)){
+	                count+=map.get(rem);
+	            }
+	            map.put(rem,map.getOrDefault(rem,0)+1);
+	        }
+	        return count;
+	    }
 }
